a996565b394dba58989b5c700c54b255c4b1408a
Tolegen Dias <tolegenisbro@gmail.com>
2023-04-14 21:31:15 +0600

    Revert "Revert "Изменил ранее добавленые изменения""
    This reverts commit 5673f87313b36c9aa33df4d0a7ef5ca7b5302f7e.

--- FoodDeliveryApp/costumer/views.py
+++ FoodDeliveryApp/costumer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.views import View
 from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
@@ -83,24 +83,4 @@ class Order(View):
             'price': price
         }
 
-        return redirect('order-confirmation',pk = order.pk)
-
-
-class OrderConfirmation(View):
-    def get(self,request,pk, *args, **kwargs):
-        order=OrderModel.objects.get(pk = pk)
-
-        context = {
-            'pk':order.pk,
-            'items':order.items,
-            'price':order.price,
-        }
-
         return render(request, 'costumer/order_confirmation.html', context)
-
-    def post(self,request,pk,*args,**kwargs):
-        print(request.body)
-
-class OrderPayConfirmation(View):
-    def get(self,request,*args,**kwargs):
-        return render(request,'costumer/order_pay_confirmation.html')
